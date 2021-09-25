2677a34b1f80ee3e37b6d9258a291d3158701d5e
liuxin <2442768863@qq.com>
2021-09-25 21:15:03 +0800

    first

---
+++ src/f.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class f {
+    public static void main(String[] args) {
+        List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        String filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining("x"));
+
+        System.out.println("筛选列表: " + filtered);
+        String mergedString = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", "));
+        System.out.println("合并字符串: " + mergedString);
+    }
+}
